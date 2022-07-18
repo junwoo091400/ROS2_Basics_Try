b8c48cf65bf146dc1f5520b45de42a63550b02bd
Junwoo Hwang <junwoo@auterion.com>
2022-07-18 21:06:57 +0200

    Use three int sum service

--- build/cpp_srvcli/CMakeFiles/server.dir/DependInfo.cmake
+++ build/cpp_srvcli/CMakeFiles/server.dir/DependInfo.cmake
@@ -17,6 +17,7 @@ set(CMAKE_TARGET_DEFINITIONS_CXX
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/junwoo/dev_ws/install/tutorial_interfaces/include"
   "/opt/ros/foxy/include"
   )
 
